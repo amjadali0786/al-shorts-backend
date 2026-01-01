94166164367aba49a427ca193e12db9fa9a814ae
amjadali0786 <amjadali8477@gmail.com>
2026-01-01 22:59:03 +0530

    Add groq and dotenv to requirements

--- app/core/database.py
+++ app/core/database.py
@@ -1,29 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.engine import URL
-from app.core.config import (
-    DB_USER,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_PORT,
-    DB_NAME,
-)
 
-DATABASE_URL = URL.create(
-    drivername="postgresql+psycopg2",
-    username=DB_USER,
-    password=DB_PASSWORD,   # @ is SAFE here
-    host=DB_HOST,
-    port=int(DB_PORT),
-    database=DB_NAME,
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-)
-
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
